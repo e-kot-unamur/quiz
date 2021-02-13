2ce368b72ce06fb70eeadc1a295def8299e9480f
Slébatman <antoine.baetsle@gmail.com>
2021-02-13 15:07:45 +0100

    add style

--- client/src/quizz/Answer.svelte
+++ client/src/quizz/Answer.svelte
@@ -1,12 +1,18 @@
 <script>
     export let answerText;
     export let checkAnswerHandler;
+    export let bgColor = '#86CAF4';
 </script>
 
 <style>
-
+    .answer{
+        display: inline-block;
+        width: 45%;
+        margin: 10px auto;
+        border-radius: 8px;
+    }
 </style>
 
-<button on:click={() => checkAnswerHandler(answerText)} class="Answer">
+<button type="button" on:click={() => checkAnswerHandler(answerText)} class="answer" style="background-color : {bgColor};">
     {answerText}
 </button>
