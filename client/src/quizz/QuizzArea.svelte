4c68d44e7ab4ba5e92f4443a256d2267c7839034
Slébatman <antoine.baetsle@gmail.com>
2021-02-14 17:26:45 +0100

    Fleme c'est quasi comme fleche

--- client/src/quizz/QuizzArea.svelte
+++ client/src/quizz/QuizzArea.svelte
@@ -94,12 +94,10 @@
 	}
 
 	.nextQuestion {
-		margin: 10px auto;
-		display: block;
-		background-color: white;
-		border : 1px solid blue;
-		border-radius: 5px;
-		text-align: center;
+		fill : black;
+		margin-top: 10px;
+		position: relative;
+		left : 85%
 	}
 
 	@media (min-width: 768px) { 
@@ -153,10 +151,9 @@
 						</svg> 
 						</button>
 					{:else }
-						<button on:click={() => navButton()} class="nextQuestion"> Question suivante <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-						<path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-						</svg> 
-						</button>
+						<svg on:click={() => navButton()} class="nextQuestion" xmlns="http://www.w3.org/2000/svg"  height="40" viewBox="0 0 16 16">
+							<path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z"/>
+						 </svg>
 					{/if}
 				</div>
 			</div>
